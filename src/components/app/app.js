3b6ff3ddb1186b456e5c7788d7ce154a1ff26b91
Giedrius <giedrius@spinas.lt>
2020-11-27 12:31:48 +0200

    pridedam "add Item" btn

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from "../app-header/app-header";
 import SearchPanel from "../search-panel/search-panel";
 import TodoList from "../todo-list/todo-list";
 import ItemStatusFilter from "../item-status-filter/item-status-filter";
+import ItemAddForm from "../item-add-form";
 
 import "./app.css";
 
-const App = () => {
-  const todoData = [
-    { label: "Gerti kava", important: false, id: 1 },
-    { label: "Kurti programa", important: true, id: 2 },
-    { label: "Dar ką nors", important: false, id: 3 }
-  ];
+export default class App extends Component {
+  maxId = 100;
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={2} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  state = {
+    todoData: [
+      { label: "Gerti kava", important: false, id: 1 },
+      { label: "Kurti programa", important: true, id: 2 },
+      { label: "Dar ką nors", important: false, id: 3 }
+    ]
+  };
+
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex(el => el.id === id);
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newArray
+      };
+    });
+  };
+
+  addItem = text => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem];
+      return {
+        todoData: newArr
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={2} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
-
-      <TodoList todos={todoData} onDeleted={id => console.log("del", id)} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
